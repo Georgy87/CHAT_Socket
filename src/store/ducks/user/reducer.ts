df8fe9e913fc78bff378fff6854833d3a8f9240c
GEORGY <goshana87@mail.ru>
2021-03-02 18:53:17 +0300

    Create userReducer.

--- src/store/ducks/user/reducer.ts
+++ src/store/ducks/user/reducer.ts
@@ -1,1 +1,33 @@
+import produce, { Draft } from "immer";
+
+export type UserStateType = {
+    data: null;
+    token: any;
+    isAuth: boolean;
+}
+
+const initialState: UserStateType = {
+    data: null,
+    token: window.localStorage.token,
+    isAuth: false
+};
+
+const userReducer = produce((draftState: Draft<UserStateType>, action: any) => {
+    switch (action.type) {
+        case "USER:SET_DATA":
+            draftState.data = action.payload;
+            draftState.isAuth = true;
+            draftState.token = window.localStorage.token;
+            break;
+        case "USER:SET_IS_AUTH":
+            draftState.isAuth = action.payload;
+            break;
+        default:
+            break;
+    }
+}, initialState);
+
+export default userReducer;
+
+
 
--- src/store/rootReducer.ts
+++ src/store/rootReducer.ts
@@ -1,5 +1,6 @@
 import { combineReducers } from "redux";
+import userReducer from './ducks/user/reducer';
 
 export const rootReducers = combineReducers({
-
+    userReducer
 });
